0cf613e197f71c8656094d9fdc0d4536781968f1
arantxamorcillo <arantxa.morcillo@gmail.com>
2021-07-26 18:37:31 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  var randomItem = function(parts) {
+    for (let i = 0; i < parts.length; i++) {
+      let randomIndex = Math.floor(Math.random() * parts.length);
+      return parts[randomIndex];
+    }
+  };
+  const excuse = function(subject, verb, object, time) {
+    return (
+      randomItem(subject) +
+      " " +
+      randomItem(verb) +
+      " " +
+      randomItem(object) +
+      " " +
+      randomItem(time)
+    );
+  };
+  document.getElementById("excuse").innerHTML = excuse(who, action, what, when);
 };
